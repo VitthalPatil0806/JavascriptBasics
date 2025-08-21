16f27b5dd3e45c0de14f3131065ca83ecd557edd
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-08-21 19:26:14 +0530

    variables code and notes

---
+++ Section01/01.Variables.js
@@ -0,0 +1,8 @@
+let num = 10
+var name = "vitthal"
+const age = 29
+address = "kolhpur"
+
+// age = 25 // not allowed to change the value of variable assigned with const keyword
+// always use let or const not var bcoz multiple same name variables defined with var was chnage value for everytime.
+console.table([num, name, age, address])
