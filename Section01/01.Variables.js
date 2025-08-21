bc174dac43ba4ae0b8b95798be2c6377f4dc6307
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-08-21 19:31:18 +0530

    comment for console table

--- Section01/01.Variables.js
+++ Section01/01.Variables.js
@@ -5,4 +5,4 @@ address = "kolhpur"
 
 // age = 25 // not allowed to change the value of variable assigned with const keyword
 // always use let or const not var bcoz multiple same name variables defined with var was chnage value for everytime.
-console.table([num, name, age, address])
+console.table([num, name, age, address]) // use table if want to console multiple variables
