e0bc526165c55d0dd34cb1f52170ec6665a58bba
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-08-24 17:23:01 +0530

    DataType Summary

---
+++ Section02/06.DTypesSummary.js
@@ -0,0 +1,27 @@
+// Primitive 
+// number, string, boolean, null, undefined, symbol, BigInt
+
+// Non Primitive
+// Array, Object, Functions
+
+// let num = 7;
+// let str = "vitthal"
+// let bool = true
+// let nullVal = null
+// let undefinedVal = undefined
+// let big = BigInt
+// let fun = function () {}
+// let arr = [1,2,3]
+// let obj = {'a':1,'b':2,'c':3}
+// let date = Date()
+
+// console.log(typeof num); // number
+// console.log(typeof str); // string
+// console.log(typeof bool); // boolean
+// console.log(typeof nullVal); // object
+// console.log(typeof undefinedVal); // undefined
+// console.log(typeof big); // function
+// console.log(typeof fun); // function
+// console.log(typeof arr); // object
+// console.log(typeof obj); // object
+// console.log(typeof date); // string
