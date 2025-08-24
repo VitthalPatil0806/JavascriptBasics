efb6e974621fbce33b632b9c0dd8378617247aee
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-08-24 11:10:59 +0530

    conversion types

---
+++ Section02/ConversionOperations.js
@@ -0,0 +1,22 @@
+// let strNum = "12abc" 
+// let convNum = Number(strNum)
+
+// console.log(typeof convNum); // type is number
+// console.log(convNum); // value is NaN
+
+// let score = null
+// let convNum = Number(score)
+
+// console.log(typeof convNum); // type is number
+// console.log(convNum); // value is 0 (for undefined value is NaN and for  true is 1 and false is 0)
+
+// let isLogIn = 1
+// let boolIsLogIn = Boolean(isLogIn)
+
+// console.log(boolIsLogIn); => value is true. (for 0 is false. for string is 1. for empty string is 0)
+
+// let num = 33
+// let strnum = String(num)
+
+// console.log(strnum); // value is 33
+// console.log(typeof strnum); // type is string
