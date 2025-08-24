4a3026e75b2e10d80ff58c7b0fe9f3fc63fc6119
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-08-24 14:33:34 +0530

    type comparison in js

---
+++ Section02/Comparison.js
@@ -0,0 +1,8 @@
+// console.log("2" > 1); // true coz treat as number
+// console.log("02" > 1); // true coz treat as number
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); // true coz null is treate as number for comparison operator
+
+// note - comparison and equal operator are both r diffrent. also try to avoid null and undefined comparison.
