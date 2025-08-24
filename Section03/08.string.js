c700f3578d1f4f879ddc4efba69cd64092f98692
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-08-24 22:25:19 +0530

    string and their methods

---
+++ Section03/08.string.js
@@ -0,0 +1,31 @@
+// in this file code of modern way of writing string is present
+
+// let name = "vitthal"
+// let repoCount = 18
+
+// console.log(name + repoCount + "hello txt");
+
+// console.log(`hello my name is ${name} and repo count is ${repoCount}`);
+// above way is writing string in string format is called interpolation.
+
+// let string = new String('patil') // we can access methods of string using this methods directly
+// console.log(string.length);
+// console.log(string[0]);
+
+// const substring = string.substring(0, 3) 
+// const substring = string.substring(-4, 2) // substring can't support on -ve index
+
+// console.log(substring);
+
+// const slice = string.slice(0, 3)
+// const slice = string.slice(-3, 4) // slice support on -ve index
+// console.log(slice);
+
+// const url = "https://vitthal.com/vitthal%20patil"
+// console.log(url);
+// console.log(url.replace('%20', '-')); // here first param is for check/search value and second one for replace it with param
+
+// console.log(url.includes('patil'));  
+
+// const str = 'vitthal-arjun-patil'
+// console.log(str.split('-')); // it break string from given param and convert it into array.
