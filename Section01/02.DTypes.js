3840fdf06627c28590b4c67d1446028471da6958
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-08-24 10:25:10 +0530

    Js datatypes

---
+++ Section01/02.DTypes.js
@@ -0,0 +1,8 @@
+// "use strict" // treat all js code as newer version
+// alert (3 + 3) // we are using nodejs not browser so different syntax for node to write alert
+
+// prmitive
+// number, bigint, string, null, undefined, boolean, symbol
+
+// console.log(typeof undefined); // type -> undefined
+// console.log(typeof null); // type -> object
