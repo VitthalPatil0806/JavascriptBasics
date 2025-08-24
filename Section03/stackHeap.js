99fe6d211c155b8ddbbb7263b222c078e43b4e6d
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-08-24 18:06:41 +0530

    stack and heap memory

---
+++ Section03/stackHeap.js
@@ -0,0 +1,20 @@
+// stack - values of all primitive datatypes are stored in stack memory
+// stack value gives copy of original value
+
+// let num = 12
+// let num2 = num
+
+// num2 = 13
+// console.log(num);
+// console.log(num2); // in this example it shows that if change is made then are not affect on assigned variable
+
+// heap - values of all non-primitive datatypes are stored in heap memory
+// heap value gives reference address of original value
+
+// let obj = {name:"vitthal", age: 25}
+// let obj2 = obj
+
+// obj2.name = "harsha"
+
+// console.log(obj.name);
+// console.log(obj2.name); // in this example it shows that if change is made then are affected on assigned variable
