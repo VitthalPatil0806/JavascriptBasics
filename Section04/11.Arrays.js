fc3cb91e805a4ed4181a012e1b07539253f51032
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-09-07 16:09:05 +0530

    array basic start

---
+++ Section04/11.Arrays.js
@@ -0,0 +1,29 @@
+const arr = [0,1,2,3,4,5]
+// console.log(arr[0]);
+
+// const arr2 = new Array(1,2,3,4)
+// console.log(arr2.length); // this new array method allows you  to access more array methods
+
+// array methods
+
+// arr.push(6)
+// console.log(arr); -> [0,1,2,3,4,5,6] // add element at last of array also unshift is same like this
+
+// arr.pop(5)
+// console.log(arr); -> [0,1,2,3,4] // remove element from last of array also shift is same like this
+
+// console.log(arr.includes(9)); // return bool val
+// console.log(arr.indexOf(9)); // return -1 for not found and if found then index of element
+
+// const newArr = arr.join()
+// console.log(arr);
+// console.log(newArr); // join return string
+
+
+// const myn1 = arr.slice(1,3) // show the array from upto before one ele. of range
+// console.log(myn1)
+// console.log('arr1',arr) // it change original array
+
+// const myn2 = arr.splice(1,3) // show the array from upto given range include
+// console.log(myn2)
+// console.log('arr2', arr); // it change original array or it manipulate original array.
