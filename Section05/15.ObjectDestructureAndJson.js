31b7a5c37da6ea357e9afe54aa8988a63a68e14d
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-09-09 18:36:15 +0530

    object destructuring

---
+++ Section05/15.ObjectDestructureAndJson.js
@@ -0,0 +1,11 @@
+const course = {
+    name: "js",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// const {price} = course
+// console.log(price); // assign single value using destructuring
+
+// const {price: amount} = course // can change variable assign name
+// console.log(amount); 
