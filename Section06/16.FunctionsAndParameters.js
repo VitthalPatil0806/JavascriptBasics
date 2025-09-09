df839a255facb8d99931ca7d00deb2a9b8397601
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-09-09 19:17:32 +0530

    function undefined

---
+++ Section06/16.FunctionsAndParameters.js
@@ -0,0 +1,29 @@
+// function AddTwoNum (num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+// console.log("sum1", AddTwoNum(5,3)); return undefined result
+
+// const sum = AddTwoNum(5,3)
+// console.log("sum", sum); //  return undefined result
+
+// function AddTwoNum (num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log("result", AddTwoNum(5,3)); // or
+// const result = AddTwoNum(5,3)
+// console.log("result", result); // return result value
+// note - code after return is unreachable means not execute
+
+// function userLoggedIn (userName) {
+//     if (!userName) {
+//         console.log('pls enter username');
+//         return
+//     }
+//     return `${userName} just logged in`
+// }
+
+// console.log(userLoggedIn('vitthal')); // -> vitthal just logged in
+// console.log(userLoggedIn()); // -> undefined just logged in
+
