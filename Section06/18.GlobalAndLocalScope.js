a632f871a7928359f4320e784113293167e8ff18
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-09-10 17:03:57 +0530

    scoping in js

---
+++ Section06/18.GlobalAndLocalScope.js
@@ -0,0 +1,48 @@
+let a = 300
+
+// if (true) {
+    // let a = 10
+//     const b = 20
+//     var c = 30
+    // console.log(a);
+// }
+
+// console.log(a); // not print
+// console.log(b); // not print
+// console.log(c); // print
+
+// function one () {
+//     const username = "vitthal"
+
+//     function two () {
+//         const wesite = "youtube"
+//         console.log(username);
+//     }
+
+//     console.log(wesite); // -> error
+//     two()
+// }
+
+// one()
+
+// if (true) {
+//     const username = "vitthal"
+//     if (true) {
+//         const website = "youtube"
+//         console.log(username + website);
+//     }
+    // console.log(website); // -> error
+// }
+// console.log(username); // -> error
+
+// hoisting lite term
+
+// console.log(addOne(5));
+// function addOne (num) {
+//     return num + 1
+// }
+
+// console.log(two(5));
+// const two = function addTwo (num) {
+//     return num + 2
+// }
