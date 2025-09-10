0649e8e7a65fa3494d2b5a3452dc7976b9c324b6
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-09-10 15:32:47 +0530

    Function of array and object

---
+++ Section06/17.FunctionObjectArray.js
@@ -0,0 +1,30 @@
+// function AddNumber (val1, val2, ...nums) {
+//     return nums
+// }
+
+// console.log(AddNumber(10, 20, 30, 40));
+
+
+// pass object as parameter to function
+// const user = {
+//     name: "vitthal",
+//     age: 25
+// }
+
+// function handleObject (obj) {
+//     console.log(`userName is ${obj.name} and age is ${obj.age}`);
+// }
+
+// handleObject(user)
+
+
+// pass array as parameter to function
+
+// const count = [10, 20, 30, 40]
+
+// function NumCount (nums) {
+//     return nums
+// }
+
+// console.log(NumCount(count));
+
