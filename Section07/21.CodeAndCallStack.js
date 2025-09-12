4c0af3c436cd30dd924cf1e0842bc0aef8aa0332
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-09-12 19:33:06 +0530

    call stack and global execusion context

---
+++ Section07/21.CodeAndCallStack.js
@@ -0,0 +1,6 @@
+// call stack - it that where actually execusion happens of code
+// global execusion context run in two phases -
+// memory creation phase - allocate memort for variable and other things in code
+// execusion phase - actual execusion of code
+
+// note - refer file no 21 for screenshot
