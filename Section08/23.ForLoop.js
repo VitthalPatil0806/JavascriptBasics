a5d34e18993389e1ff60cd948565783e11b7af27
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-09-13 22:44:02 +0530

    for loop

---
+++ Section08/23.ForLoop.js
@@ -0,0 +1,9 @@
+let arr = ['val1', 'val2', 'val3']
+// for (let i = 0; i<= arr.length; i++) {
+//     console.log(arr[i]); // fourth value come is undefined coz we give <= condition here
+// }
+
+// for (let i = 0; i < arr.length; i) {
+// console.log(arr[i]); // it only print first ele. of array coz condition not change here -> i
+// }
+
