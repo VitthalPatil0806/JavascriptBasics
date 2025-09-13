e61a7c0b38f7ff5052f00c26ff4d6423889ee866
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-09-13 19:24:58 +0530

    control flow notes

---
+++ Section08/22.ControlFlow.js
@@ -0,0 +1,23 @@
+// other things are same as it is like if-else, switch, nested if-else
+
+// nullish coalescing operator -
+// this ?? is used to if null or undefined value is come then to handle it.
+
+
+// Falsy Value -
+// false (the boolean primitive)
+// 0 (the number zero)
+// -0 (negative zero)
+// 0n (BigInt zero)
+// "", '', `` ` `` (empty strings)
+// null
+// undefined
+// NaN (Not-a-Number)
+
+// Truthy Value -
+// true (the boolean primitive)
+// Any non-zero number (e.g., 42, -1, 3.14)
+// Any non-empty string (e.g., "hello", "false", "0")
+// Objects (e.g., {}, [], new Date(), functions)
+// Symbols
+// BigInt values other than 0n
