aac70c49c6627ee4401fc92648c447546df7427b
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-09-14 19:00:19 +0530

    loops in js

---
+++ Section08/23.Loops.js
@@ -0,0 +1,29 @@
+let arr = ['val1', 'val2', 'val3']
+// for (let i = 0; i<= arr.length; i++) {
+//     console.log(arr[i]); // fourth value come is undefined coz we give <= condition here
+// }
+
+// for (let i = 0; i < arr.length; i) {
+// console.log(arr[i]); // it only print first ele. of array in infinite loop coz condition not change here -> i
+// }
+
+// while loop
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArr = ['flash', 'batman', 'spiderman']
+
+// let index = 0
+// while (index < myArr.length) {
+//     console.log(`value of index is ${myArr[index]}`);
+//     index ++;
+// }
+
+// let score = 11
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+// } while(score <= 10)
