bf34f7f84081cba3a2ef6ce69d4b374c02ab8439
Sheetal Patil <sheetalpatil1009@gmail.com>
2025-09-21 19:22:39 +0530

    async fundamentals

---
+++ Section11/31.AsyncJs.js
@@ -0,0 +1,3 @@
+// JS is syncronous lang. it it also single threaded. 
+// Blocking code - run syncronously use case is to read file data in os
+// Non-Blocking code - run asyncronously use case is to give success msg after saving saving data into db
