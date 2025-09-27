9c5e2015bff0158c13669f9a9964c2e2d0ecc912
Prashant2620 <jadhavprashant2620@gmail.com>
2025-09-27 21:55:10 +0530

    call in js

---
+++ Section13/38.Call.js
@@ -0,0 +1,13 @@
+// function SetUserName (username) {
+// this.username = username
+// return username
+// }
+
+// function createUser (username, email, password) {
+//     SetUserName.call(this, username) // call used to pass current execution context to another function
+//     this.email = email
+//     this.password = password
+// }
+
+// const chai = new createUser("chai", "chai@fb.com", 1234)
+// console.log(chai);
